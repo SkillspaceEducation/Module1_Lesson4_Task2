4ca5d509557d7e0a354f0d3a3a2e7ed52f31d1e5
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-03-19 09:50:41 +0300

    Создайте переменную для массива из 10 элементов. Заполните его произвольными значениями целочисленного типа. Найдите максимальный элемент и выведите его индекс.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+/*
+Создайте переменную для массива из 10 элементов.
+Заполните его произвольными значениями целочисленного типа.
+Найдите максимальный элемент и выведите его индекс.
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final int arrayInitLen = 10;
+        int[] arrayInit = new int[arrayInitLen];
+        Random rand = new Random();
+        for (int i = 0; i < arrayInit.length; i++) {
+            arrayInit[i] = rand.nextInt(50);
+        }
+        System.out.println("Исходный массив\n" + Arrays.toString(arrayInit));
+        int maxRand = 0;
+        int numMaxRand = 0;
+        for (int i = 0; i < arrayInit.length; i++) {
+            if (arrayInit[i] > maxRand) { // Находим максимальный элемент массива и его индекс
+                maxRand = arrayInit[i];
+                numMaxRand = i;
+            }
+        }
+        System.out.printf("Максимальный элемент массива имеет индекс %d и равен %d", numMaxRand, maxRand);
     }
 }
